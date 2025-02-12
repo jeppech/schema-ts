a06313c2e90f108c8e97a0b945256f9ecd88128e
Jeppe Christiansen <jeppe@codr.dk>
2025-02-12 13:30:37 +0100

    chore: comments

--- src/validators.ts
+++ src/validators.ts
@@ -1,5 +1,8 @@
 import { ValidationError } from './errors.js';
 
+/**
+ * The value must be an exact match of `expected`.
+ */
 export function literal(expected: unknown, err = 'invalid value') {
   return (value: unknown, field: string) => {
     if (value !== expected) {
@@ -8,6 +11,9 @@ export function literal(expected: unknown, err = 'invalid value') {
   };
 }
 
+/**
+ * The value must be between `min` and `max`.
+ */
 export function minmax(min: number, max: number, err = 'out of range') {
   return (value: number, field: string) => {
     if (value < min || value > max) {
@@ -16,6 +22,9 @@ export function minmax(min: number, max: number, err = 'out of range') {
   };
 }
 
+/**
+ * The value may not be shorter than `min` or longer than `max`.
+ */
 export function length(min: number, max: number, err = 'invalid length') {
   return (value: string, field: string) => {
     if (value.length < min || value.length > max) {
@@ -24,6 +33,9 @@ export function length(min: number, max: number, err = 'invalid length') {
   };
 }
 
+/**
+ * The value must be a valid email address.
+ */
 export function email(err = 'expected an email') {
   return (value: string, field: string) => {
     if (!value.match(/^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/)) {
@@ -32,6 +44,9 @@ export function email(err = 'expected an email') {
   };
 }
 
+/**
+ * The value must be a valid formatted imei.
+ */
 export function imei(err = 'invalid formatted imei') {
   return (value: string, field: string) => {
     if (!value.match(/^[0-9]{2}s?[0-9]{6}s?[0-9]{6}s?[0-9]{1,3}$/)) {
