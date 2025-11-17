4951eed8b6314c6447dd01b6449da70976e31fbf
Jeppe Christiansen <jeppe@codr.dk>
2025-11-17 14:45:06 +0100

    chore: use zod email regex

--- src/validators.ts
+++ src/validators.ts
@@ -42,7 +42,7 @@ export function length(min: number, max: number, err = SchemaErrors.invalid_leng
  */
 export function email(err = SchemaErrors.invalid_formatted_email) {
   return (value: string, field: string) => {
-    if (!value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
+    if (!value.match(/^(?!\.)(?!.*\.\.)([a-z0-9_'+\-\.]*)[a-z0-9_'+\-]@([a-z0-9][a-z0-9\-]*\.)+[a-z]{2,}$/)) {
       return new ValidationError(err, value, field);
     }
   };
